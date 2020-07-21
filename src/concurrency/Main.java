85353a7d32b21b8d89685c8da4569efd997fa9e9
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-21 16:29:31 -0500

    Abigail swigert (#1)
    * completed exercise 1 parts 1 and 2
    
    * completed exercise 1 part 3
    
    * completed exercise 1 bonus for TeamTC1 class
    
    * almost all instance methods have been completed in Numbers
    
    * exercise 2 part 1 finished
    
    * completed exercise 2
    
    * completed exercise 3

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -4,5 +4,12 @@ public class Main {
    
     public static void main(String[] args) {
          //run all of your threads from this main class.
+        Reasoning reasoning = new Reasoning();
+        reasoning.start();
+
+        TeamTC1 teamTC1 = new TeamTC1();
+        Thread teamV8 = new Thread(teamTC1);
+        teamV8.setName("Team V8");
+        teamV8.start();
     }
 }
--- src/concurrency/Reasoning.java
+++ src/concurrency/Reasoning.java
@@ -1,10 +1,18 @@
 package concurrency;
 
-class Reasoning {
+class Reasoning extends Thread {
     //set up this class so it can become a valid thread. 
     void distinguish() {
         //print to the console the difference between a thread and a process
         //print out you think will happen if you invoke the run() method of a thread as opposed to the start() method of a thread.
+
+        System.out.println("Threads have shared memory and run in a shared memory space, whereas processes run in separate memory spaces.");
+        System.out.println("If you call run(), the code inside the body of the run method will be executed on the current thread. If you call start(), a new thread will be created and the run method will be executed in the new thread.");
+    }
+
+    @Override
+    public void run() {
+        this.distinguish();
     }
 }
 
